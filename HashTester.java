5421ae28fd04828bef7840364dde3ec0c86822fc
Yeswanth Yerra <70848085+yash1th-yerra@users.noreply.github.com>
2024-10-10 16:23:26 +0000

    Custom-HashMap v2.0

--- HashTester.java
+++ HashTester.java
@@ -3,12 +3,14 @@ import java.util.List;
 public class HashTester {
     public static void main(String[] args) {
         HashCard<Integer,String> hashCard = new HashCard<>();
+
+        // This is my testing implementation
         hashCard.put(1,"a");
         hashCard.put(2,"b");
         hashCard.put(3,"c");
         hashCard.put(4,"f");
         hashCard.put(5,"d");
-
+        
         System.out.println(hashCard.get(5));
         System.out.println(hashCard.get(2));
         System.out.println(hashCard.get(3));
